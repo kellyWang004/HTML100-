e9d666760029c603ce68cef72758d8d62c81a077
KellyWang <2754422056@qq.com>
2022-04-01 15:28:20 +0800

    2022.04.01下午（基本完成）

--- html100道练习题/main.py
+++ html100道练习题/main.py
@@ -1,5 +1,5 @@
 import os
-for i in range(110,120):
+for i in range(130,140):
     with open(f"{i}.html","w",encoding="UTF-8") as file:
-        with open("109.html","r",encoding="UTF-8") as file2:
+        with open("129.html","r",encoding="UTF-8") as file2:
             file.write(file2.read())
